77e5b04ef67dd52a598195187d4ad82a28219b5a
Social Complexity of Climate Change <83168418+SC3-TUD@users.noreply.github.com>
2023-09-12 17:12:04 +0200

    Update research_and_development.py

--- research_and_development.py
+++ research_and_development.py
@@ -40,9 +40,6 @@ def calculate_RD_budget(sales, net_worth, rd_fraction=0.04, in_fraction=0.5):
     """
 
     # Determine total RD budget based on sales or net worth
-
-    # COMMENT: function only called when sales > 0 (in CapitalGoodFirm class)
-    #          --> change or remove this check from function, now rest does nothing
     if sales > 0:
         rd_budget = rd_fraction * sales
     elif net_worth < 0:
@@ -106,11 +103,7 @@ def imitate(firm, Z=0.3, e=5):
                                       other_firm.productivity[1], 2)))
             if distance == 0:
                 IM_prob.append(0)
-            # elif other_firm.region != firm.region:
-            #     # Add geographical distance if firm is in other region
-            #     # COMMENT: when readding second region: shouldn't this
-            #     #          be (1/ (e*distance))?
-            #     IM_prob.append(1/e * distance)
+         
             else:
                 IM_prob.append(1 / distance)
 
@@ -126,88 +119,4 @@ def imitate(firm, Z=0.3, e=5):
     return im_productivity
 
 
-# # --------
-# # COMMENT: same as above?
-# # --------
-# def calculateRDBudgetCCA(sales, net_worth, v=0.005, e=0.5):
-#     if sales > 0:
-#         rd_budget = v * sales
-#     elif net_worth < 0:
-#         rd_budget = 0
-#     else:
-#         rd_budget = v * net_worth
 
-#     in_budget = e * rd_budget
-#     im_budget = (1 - e) * rd_budget
-
-#     return rd_budget, in_budget, im_budget
-
-
-# # --------
-# # COMMENT: same as above?
-# # --------
-# def innovate_CCA(IN, R, Z=0.3, a=3, b=3, x_low=-0.10, x_up=0.10):
-#     """
-#     RD : CCA resilience coefficient
-#     """
-#     in_R = [0, 0]
-
-#     # Bernoulli draw to determine success (1) or failure (0)
-#     p = 1 - math.exp(-Z * IN / 2)
-#     # Labor productivity resilience
-#     if bernoulli.rvs(p) == 1:
-#         # New resilience coefficient from innovation
-#         in_R[0] = R[0] * (1 + x_low + beta.rvs(a, b) * (x_up - x_low))
-
-#     # Capital stock resilience
-#     if bernoulli.rvs(p) == 1:
-#         in_R[1] = R[1] * (1 + x_low + beta.rvs(a, b) * (x_up - x_low))
-
-#     return in_R
-
-
-# # --------
-# # COMMENT: same as above?
-# # --------
-# def imitate_CCA(IM, firm_ids, agents, R, reg, Z=0.3, e=1.5):
-#     """CCA RD: imitation
-#     """
-#     im_R = [0, 0]
-
-#     # Bernoulli draw to determine success (1) or failure (0)
-#     p = 1 - math.exp(-Z * IM)
-#     if bernoulli.rvs(p) == 1:
-#         # Compute inverse Euclidean distances
-#         imiProb = []
-#         imiProbID = []
-#         for id in firm_ids:
-#             firm = agents[id]
-#             distance = (math.sqrt(pow(R[0] - firm.CCA_resilience[0], 2) +
-#                         pow(R[0] - firm.CCA_resilience[0], 2)))
-#             if distance == 0:
-#                 imiProb.append(0)
-#             else:
-#                 # Increase distance if the firm is in another region
-#                 if firm.region != reg:
-#                     imiProb.append(1/e*distance)
-#                 else:
-#                     imiProb.append(1/distance)
-#             imiProbID.append(firm.unique_id)
-
-#         # Cumulative probability
-#         if (sum(imiProb) > 0):
-#             acc = 0
-#             for i in range(len(imiProb)):
-#                 acc += imiProb[i] / sum(imiProb)
-#                 imiProb[i] = acc
-
-#             # Randomly pick a firm to imitate (index j)
-#             rnd = random.uniform(0, 1)
-#             j = bisect.bisect_right(imiProb, rnd)
-#             # Copy that firm's technology
-#             if j < len(imiProb):
-#                 firm = agents[imiProbID[j]]
-#                 im_R[0] = firm.CCA_resilience[0]
-#                 im_R[1] = firm.CCA_resilience[1]
-
-#     return im_R
